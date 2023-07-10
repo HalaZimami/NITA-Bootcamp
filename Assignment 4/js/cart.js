e5da4a6a6d44a7b3481e191b4eb9efc898bbcd25
hala <halazimami@gmail.com>
2023-07-10 15:00:45 +0300

    last update

--- Assignment 4/js/cart.js
+++ Assignment 4/js/cart.js
@@ -1,81 +1,222 @@
-// let openShopping = document.querySelector('.shopping');
-// let closeShopping = document.querySelector('.closeShopping');
-// let body = document.querySelector('body');
+$(document).ready(function () {
+    $(".card").slice(0, 8).show();
+    $("#loadMore").on("click", function (e) {
+        e.preventDefault();
+        $(".card:hidden").slice(0, 8).slideDown();
+        if ($(".card:hidden").length === 0) {
+            $("#loadMore").text(" ").addClass("noContent");
+        }
+    });
 
+})
+
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let body = document.querySelector('body');
 let list = document.querySelector('.list');
 let list2 = document.querySelector('.list2');
-// let listCard = document.querySelector('.listCard');
-// let total = document.querySelector('.total');
-// let quantity = document.querySelector('.quantity');
+let listCard = document.querySelector('.listCard');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
 
-// openShopping.addEventListener('click', ()=>{
-//     body.classList.add('active');
-// })
-// closeShopping.addEventListener('click', ()=>{
-//     body.classList.remove('active');
-// })
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
 
 
 let products = [
+    // {
+    //     id: 1,
+    //     reviews: 'Star-products.png',
+    //     fav: 'empty-heart.png',
+    //     name: 'Arabica Coffee Beans #1',
+    //     image: 'product.png',
+    //     price: 13
+    // },
+    // {
+    //     id: 2,
+    //     reviews: 'Star-products.png',
+    //     fav: 'red-heart.png',
+    //     name: 'Robusta Coffee Beans #2',
+    //     image: 'product.png',
+    //     price: 13
+    // },
+    // {
+    //     id: 3,
+    //     reviews: 'Star-products.png',
+    //     fav: 'empty-heart.png',
+    //     name: 'Liberica Coffee Beans #3',
+    //     image: 'product.png',
+    //     price: 13
+    // },
+    // {
+    //     id: 4,
+    //     reviews: 'Star-products.png',
+    //     fav: 'empty-heart.png',
+    //     name: 'Excelsa Coffee Beans #4',
+    //     image: 'product.png',
+    //     price: 13
+    // },
+    // {
+    //     id: 5,
+    //     reviews: 'Star-products.png',
+    //     fav: 'red-heart.png',
+    //     name: 'Arabica Coffee Beans #5',
+    //     image: 'product.png',
+    //     price: 13
+    // },
     {
-        id: 1,
+        id: "One",
         reviews: 'Star-products.png',
         fav: 'empty-heart.png',
-        name: 'Arabica Coffee Beans',
+        name: 'Robusta Coffee Beans #1',
         image: 'product.png',
         price: 13
     },
     {
-        id: 2,
-        reviews: 'Star-products.png',
-        fav: 'red-heart.png',
-        name: 'Robusta Coffee Beans',
-        image: 'product.png',
-        price: 13
-    },
-    {
-        id: 3,
+        id: "Tow",
         reviews: 'Star-products.png',
         fav: 'empty-heart.png',
-        name: 'Liberica Coffee Beans',
+        name: 'Robusta Coffee Beans #2',
         image: 'product.png',
         price: 13
     },
     {
-        id: 4,
+        id: "Three",
         reviews: 'Star-products.png',
         fav: 'empty-heart.png',
-        name: 'Excelsa Coffee Beans',
-        image: 'product.png',
-        price: 13
-    },
-    {
-        id: 5,
-        reviews: 'Star-products.png',
-        fav: 'red-heart.png',
-        name: 'Arabica Coffee Beans',
-        image: 'product.png',
-        price: 13
-    },
-    {
-        id: 6,
-        reviews: 'Star-products.png',
-        fav: 'empty-heart.png',
-        name: 'Robusta Coffee Beans',
-        image: 'product.png',
-        price: 13
-    },
-    {
-        id: 7,
-        reviews: 'Star-products.png',
-        fav: 'empty-heart.png',
-        name: 'Robusta Coffee Beans',
+        name: 'Robusta Coffee Beans #3',
         image: 'product.png',
         price: 13
     }
+    ,
+    {
+        id: "Four",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Robusta Coffee Beans #4',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: "Five",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #5',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: "Six",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #6',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: "Seven",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #7',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: "Eight",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #8',
+        image: 'product.png',
+        price: 13
+    },
+    ,
+    {
+        id: "Nine",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #9',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: "Ten",
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #10',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 11,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #11',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 12,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #12',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 13,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #13',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 14,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #14',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 15,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #15',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 16,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #16',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 17,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #17',
+        image: 'product.png',
+        price: 13
+    },
+    {
+        id: 18,
+        reviews: 'Star-products.png',
+        fav: 'empty-heart.png',
+        name: 'Excelsa Coffee Beans #18',
+        image: 'product.png',
+        price: 13
+    },
 ];
 
-// let listCards = [];
+let listCards = [];
 function initApp() {
     products.forEach((value, key) => {
         let newDiv = document.createElement('div');
@@ -100,11 +241,16 @@ function initApp() {
             ;
         list.appendChild(newDiv);
     })
-    for(let i=0; i<6 ; i++){
-        let newDiv2 = document.createElement('div');
-        newDiv2.classList.add('item');
-        newDiv2.innerHTML = `
-        <div class="card-best">
+}
+
+function init2(){
+    products.forEach((value, key) => {
+        if(value.id <6){
+        console.log(value)
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+        <div class="card">
         <div class="product-icons">
             <img src="images/${value.reviews}">
             <img src="images/${value.fav}">
@@ -121,58 +267,60 @@ function initApp() {
             </div>
             </div>`
             ;
-        list2.appendChild(newDiv2);
+        list.appendChild(newDiv);
     }
-
+        
+    })
 }
 
+init2();
 initApp();
 
-// function addToCard(key){
-//     if(listCards[key] == null){
-//         // copy product form list to list card
-//         listCards[key] = JSON.parse(JSON.stringify(products[key]));
-//         listCards[key].quantity = 1;
-//     }
-//     if(listCards[key] == null){
-//         // copy product form list to list card
-//         listCards[key] = JSON.parse(JSON.stringify(bestseller[key]));
-//         listCards[key].quantity = 1;
-//     }
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(bestseller[key]));
+        listCards[key].quantity = 1;
+    }
 
-//     reloadCard();
-// }
+    reloadCard();
+}
 
-// function reloadCard(){
-//     listCard.innerHTML = '';
-//     let count = 0;
-//     let totalPrice = 0;
-//     listCards.forEach((value, key)=>{
-//         totalPrice = totalPrice + value.price;
-//         count = count + value.quantity;
-//         if(value != null){
-//             let newDiv = document.createElement('li');
-//             newDiv.innerHTML = `
-//                 <div><img src="images/${value.image}"/></div>
-//                 <div>${value.name}</div>
-//                 <div>${value.price.toLocaleString()}</div>
-//                 <div>
-//                     <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-//                     <div class="count">${value.quantity}</div>
-//                     <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
-//                 </div>`;
-//                 listCard.appendChild(newDiv);
-//         }
-//     })
-//     total.innerText =  `$${totalPrice.toLocaleString()}`;
-//     quantity.innerText = count;
-// }
-// function changeQuantity(key, quantity){
-//     if(quantity == 0){
-//         delete listCards[key];
-//     }else{
-//         listCards[key].quantity = quantity;
-//         listCards[key].price = quantity * products[key].price;
-//     }
-//     reloadCard();
-// }
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="images/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText =  `$${totalPrice.toLocaleString()}`;
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
